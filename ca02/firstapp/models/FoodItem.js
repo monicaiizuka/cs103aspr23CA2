32a722b663db0276f25e832ec26280828671546b
abigailmurphy <abigailmurphy@brandeis.edu>
2023-05-03 23:05:17 -0400

    Create FoodItem.js

---
+++ ca02/firstapp/models/FoodItem.js
@@ -0,0 +1,15 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var toDoItemSchema = Schema( {
+  destination: String,
+  duration: Number,
+  budget: Number,
+  yum: String,
+  yuck: String,
+  userId: {type:ObjectId, ref:'user' }
+} );
+
+module.exports = mongoose.model( 'FoodItem', foodItemSchema );
