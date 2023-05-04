284845c31b1ff46235e7fdb17075e2602bc1f312
monicaiizuka <123885703+monicaiizuka@users.noreply.github.com>
2023-05-03 22:25:49 -0400

    Create attractions.js

---
+++ ca02/firstapp/routes/attractions.js
@@ -0,0 +1,21 @@
+*
+  travel.js -- Router for the attractionsRouter
+*/
+const express = require('express');
+const router = express.Router();
+const Travel = require('../models/Travel')
+const User = require('../models/User')
+
+router.get('/travel', (req,res,next) => {
+  res.render('travel')
+})
+
+isLoggedIn = (req,res,next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+module.exports = router;
